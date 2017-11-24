c989268b65507449e909b00d2db3e39ca3dd206d
noer21 <noer.herlambang@gmail.com>
2017-11-24 06:19:59 -0800

    create dbconnect, 24-11 21:19

--- pages/backend/home.php
+++ pages/backend/home.php
@@ -1,11 +1,12 @@
 <?php
-    session_start();
-    $connect = mysqli_connect('localhost', 'root', '', 'go-event');
+    
+    include('dbconnect.php');
+
 
     $user_id = $_GET['user_id'];
     $category = "SELECT c.category_id FROM user_category AS uc WHERE uc.user_id = '$user_id'";
     $event = "SELECT * FROM events as e INNER JOIN event_category as ec ON e.event_id = ec.event_id WHERE ec.category.id = '$category' LIMIT 15";
 
-    
+
 
 ?>
