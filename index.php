5144d26b3636fa19daa137a77180aba10f80a353
noer21 <noer.herlambang@gmail.com>
2017-11-25 02:02:36 -0800

    nyatuin tombol sign in google

---
+++ index.php
@@ -0,0 +1,176 @@
+<?php
+  include('pages/backend/home.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="utf-8" />
+  <meta name="HVSZ" content="GoEvent" />
+
+  <title>GoEvent</title>
+
+  <link rel="shortcut icon" href="images/title.png" />
+  
+  <!-- pengaturan view agar responsif namun pada mobile tidak bisa zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  
+  <!-- attach JavaScripts -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <!--<script src="//maps.google.com/maps/api/js?sensor=true"></script>-->
+  <!--<script src="js/main.js"></script>-->
+
+  <!-- attach CSS styles -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style_goevent.css" rel="stylesheet" />
+
+  <!-- Online attachment - offline doesnt work -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<!--<script type="text/javascript" src="js/bootstrap.min.js"></script> -->
+
+<body>
+  <!-- NavBar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" id="goevent_color" href="index.html">GoEvent</a>
+          </div>
+      
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Browse Event</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Organize <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Overview</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">Certificate</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">Pricing</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">Resources</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Where are my tickets?</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">How to contact event organizer</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Help Center</a></li>
+                </ul>
+              </li>
+              <li><a href="pages/sign_in">Sign In</a></li>
+              <li><a href="#">Create Event</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <!-- Search -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-8">
+        <h1 class="text-center">Find your next experience</h1>
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search events or categories">
+          <span class="input-group-btn">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Date <span class="caret"></span>
+              </button>
+                <ul class="dropdown-menu">
+                  <li><a href="#">All Dates</a></li>
+                  <li><a href="#">Today</a></li>
+                  <li><a href="#">Tomorrow</a></li>
+                  <li><a href="#">This Week</a></li>
+                  <li><a href="#">This Weekend</a></li>
+                  <li><a href="#">Next Week</a></li>
+                  <li><a href="#">Next Month</a></li>
+                </ul>
+            <button class="btn btn-default" type="button">SEARCH</button>
+          </span>
+        </div><!-- /input-group -->
+      </div><!-- /.col-lg-6 -->
+      <div class="col-lg-2"></div>
+    </div><!-- /.row -->
+  </div>
+
+  <!-- Nearest event -->
+  <div class="container">
+    <div class="text-center">
+      <p>Events for you in <a>jakarta</a></p>
+    </div>  
+  </div>  
+
+  <!-- Events Card -->
+  <div class="container">
+    <div class="row">
+      <?php while ($item = mysqli_fetch_array($event)) { ?>
+        <div class="col-lg-4">
+          <p>
+            <?php echo $item['event_name']; ?> </br>
+            <?php echo $item['event_city']; ?> </br>
+          </p>
+        </div>
+      <?php } ?>
+    </div>
+  </div>
+
+  <div class="clearfix"> 
+  </div>
+  
+  <!-- button see more -->
+  <div class="container text-center">
+    <a href="pages/browse_event" class="btn btn-primary btn-round btn-block">See More</a>
+  </div>
+
+  <!-- browse by kategories -->
+  <div class="container">
+    <h2 class="text-center">Browse by Top Categories</h1>
+      <div class="row">
+      <div class="col-lg-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="col-lg-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="col-lg-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="col-lg-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="col-lg-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="col-lg-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- footer -->
+  <footer>
+    <hr />
+    <div class="container">
+      <p class="text-center">Copyright @GoEvent   HVSZ 2017</p>
+    </div>
+  </footer>
+  
+</body>
+</html>
--- pages/backend/home.php
+++ pages/backend/home.php
@@ -2,11 +2,14 @@
     
     include('dbconnect.php');
 
-
     $user_id = $_GET['user_id'];
-    $category = "SELECT c.category_id FROM user_category AS uc WHERE uc.user_id = '$user_id'";
-    $event = "SELECT * FROM events as e INNER JOIN event_category as ec ON e.event_id = ec.event_id WHERE ec.category.id = '$category' LIMIT 15";
-
+    if ($user_id != NULL){
+        $category = "SELECT c.category_id FROM user_category AS uc WHERE uc.user_id = '$user_id'";
+        $event_query = "SELECT * FROM events as e INNER JOIN event_category as ec ON e.event_id = ec.event_id WHERE ec.category.id = '$category' LIMIT 15";
+    }
+    else
+        $event_query = "SELECT * FROM events";
+    $event = mysqli_query($connect, $event_query);
     if (isset($_POST['see_more']))
         header('location: ../browse_event/index.html');
     
