00a89ea1854164f74df585c9fc485e6558fa81b4
nuhsat <nuhsatria@gmail.com>
2017-11-25 16:42:44 +0700

    login google, 25-11-17, 16:42

---
+++ pages/backend/sign_in_google/User.php
@@ -0,0 +1,45 @@
+<?php
+class User {
+	private $dbHost     = "localhost";
+    private $dbUsername = "root";
+    private $dbPassword = "";
+    private $dbName     = "go-event";
+    private $userTbl    = 'user';
+	
+	function __construct(){
+        if(!isset($this->db)){
+            // Connect to the database
+            $conn = new mysqli($this->dbHost, $this->dbUsername, $this->dbPassword, $this->dbName);
+            if($conn->connect_error){
+                die("Failed to connect with MySQL: " . $conn->connect_error);
+            }else{
+                $this->db = $conn;
+            }
+        }
+    }
+	
+	function checkUser($userData = array()){
+        if(!empty($userData)){
+            //Check whether user data already exists in database
+            $prevQuery = "SELECT * FROM ".$this->userTbl." WHERE user_uid = '".$userData['user_uid']."'";
+            $prevResult = $this->db->query($prevQuery);
+            if($prevResult->num_rows > 0){
+                //Update user data if already exists
+                $query = "UPDATE ".$this->userTbl." SET user_name = '".$userData['user_name']."', user_email = '".$userData['user_email']."', user_city = '".$userData['user_city']."', user_photo = '".$userData['user_photo']."' WHERE user_uid = '".$userData['user_uid']."'";
+                $update = $this->db->query($query);
+            }else{
+                //Insert user data
+                $query = "INSERT INTO ".$this->userTbl." SET user_uid = '".$userData['user_uid']."', user_name = '".$userData['user_name']."', user_email = '".$userData['user_email']."', user_city = '".$userData['user_city']."', user_photo = '".$userData['user_photo']."'";
+                $insert = $this->db->query($query);
+            }
+            
+            //Get user data from the database
+            $result = $this->db->query($prevQuery);
+            $userData = $result->fetch_assoc();
+        }
+        
+        //Return user data
+        return $userData;
+    }
+}
+?>
