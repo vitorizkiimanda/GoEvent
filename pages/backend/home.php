98140b3333c08bcc714088758a8fbcdf62201b98
noer21 <noer.herlambang@gmail.com>
2017-11-26 01:24:39 -0800

    wajib

--- pages/backend/home.php
+++ pages/backend/home.php
@@ -7,6 +7,8 @@
     $category = "SELECT c.category_id FROM user_category AS uc WHERE uc.user_id = '$user_id'";
     $event = "SELECT * FROM events as e INNER JOIN event_category as ec ON e.event_id = ec.event_id WHERE ec.category.id = '$category' LIMIT 15";
 
-
+    if (isset($_POST['see_more'])){
+        header('location: index.html');
+    }
 
 ?>
