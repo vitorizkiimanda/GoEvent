61f7292ece752a4478e741a1e8bcf8a0d30dfc5e
zulfahmi14 <ibnuhadi11@gmail.com>
2017-12-13 03:53:45 +0700

    edit photo event

--- pages/backend/edit_event.php
+++ pages/backend/edit_event.php
@@ -25,27 +25,27 @@
    $event_type = $_POST['event_type'];
    $event_topic = $_POST['event_topic'];
 
-       $photo_name = $_FILES['organizer_photo']['name'];
-       $photo_size = $_FILES['organizer_photo']['size'];
-       $photo_type = $_FILES['organizer_photo']['type'];
-       $photo_file = $_FILES['organizer_photo']['tmp_name'];
+       $photo_name = $_FILES['event_photo']['name'];
+       $photo_size = $_FILES['event_photo']['size'];
+       $photo_type = $_FILES['event_photo']['type'];
+       $photo_file = $_FILES['event_photo']['tmp_name'];
        $sub_photo = substr($photo_type,6);
        $photo_name = $event_id.'_photo.'.$sub_photo;
-       $photo_path = "../../photo_organizer/".$photo_name;
+       $photo_path = "../../photo_event/".$photo_name;
        move_uploaded_file($photo_file, $photo_path);
-
+       if( file_exists($_FILES['organizer_photo']['tmp_name']) && is_uploaded_file($_FILES['organizer_photo']['tmp_name'])){
         $sql = "UPDATE events SET event_name = '$event_name' ,
                           event_city = '$event_city' ,
-                          event_date_starts = '$event_date_starts'
-                          event_time_starts = '$event_time_starts'
-                          event_date_ends = '$event_date_ends'
-                          event_time_ends = '$event_time_ends'
-                          event_capacity = '$event_capacity'
-                          event_description = '$event_description' ,
+                          event_date_starts = '$event_date_starts',
+                          event_time_starts = '$event_time_starts',
+                          event_date_ends = '$event_date_ends',
+                          event_time_ends = '$event_time_ends',
+                          event_capacity = '$event_capacity',
+                          event_description = '$ckeditor' ,
                           event_video = '$event_video' ,
                           event_photo = '$photo_name' ,
                           ticket_name = '$ticket_name' ,
-                          ticket_quantity = '$ticket_quantity'
+                          ticket_quantity = '$ticket_quantity',
                           ticket_price  = '$ticket_price' ,
                           ticket_description = '$ticket_description' ,
                           ticket_date_starts = '$ticket_date_starts' ,
@@ -55,6 +55,29 @@
                           event_type = '$event_type' ,
                           event_topic = '$event_topic'
                           WHERE event_id = '$event_id'";
+        }
+        else {
+          $sql = "UPDATE events SET event_name = '$event_name' ,
+                            event_city = '$event_city' ,
+                            event_date_starts = '$event_date_starts',
+                            event_time_starts = '$event_time_starts',
+                            event_date_ends = '$event_date_ends',
+                            event_time_ends = '$event_time_ends',
+                            event_capacity = '$event_capacity',
+                            event_description = '$ckeditor' ,
+                            event_video = '$event_video' ,
+                            ticket_name = '$ticket_name' ,
+                            ticket_quantity = '$ticket_quantity',
+                            ticket_price  = '$ticket_price' ,
+                            ticket_description = '$ticket_description' ,
+                            ticket_date_starts = '$ticket_date_starts' ,
+                            ticket_time_starts = '$ticket_time_starts' ,
+                            ticket_date_ends = '$ticket_date_ends' ,
+                            ticket_time_ends = '$ticket_time_ends' ,
+                            event_type = '$event_type' ,
+                            event_topic = '$event_topic'
+                            WHERE event_id = '$event_id'";
+        }
         if ($connect->query($sql) === true) {
             echo "<p> New Event Successfully Created</p>";
             header('Location: ../organizer_profile_choose_create');
